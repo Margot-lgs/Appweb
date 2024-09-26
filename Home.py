0841bb51e234dfe6a5f5e6afed1cc06176a8df97
Margot-lgs <margot.langlais@eemi.com>
2024-09-26 14:28:02 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,3 +13,9 @@ st.write(user_input)
 
 # Image
 st.image("https://tse3.mm.bing.net/th?id=OIP.Fzx8LSN_XuDpC_-Hw1mu6QHaKa&pid=Api")
+
+# Vidéo
+st.video
+
+# Sidebar
+st.sidebar.title("Margot Langlais")
