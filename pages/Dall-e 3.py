e46be02fd934c5d899bc6ad57a729f2b36499e4f
Margot-lgs <margot.langlais@eemi.com>
2024-09-26 15:31:26 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -5,5 +5,18 @@ st.title("Dall-e 3")
 st.write("Ceci est un exercice")
 
 user_input = st.text_input ("Tapez votre texte : ")
-
 st.write(user_input)
+
+client = OpenAI(api_key=OpenAIKEY)
+prompt = "une petite loutre entourée de coquillage"
+
+image = client.images.generate(
+    model="dall-e-3",
+    prompt=prompt,
+    size="1024x1024",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
+print(image_url)
