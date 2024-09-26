ada989800df2db23cfe93697f80646b297ee9b28
Margot-lgs <margot.langlais@eemi.com>
2024-09-26 14:20:58 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,3 +10,6 @@ st.write("Ceci est un formulaire de contact")
 user_input = st.text_input ("Tapez votre texte : ")
 
 st.write(user_input)
+
+# Image
+st.image("https://tse3.mm.bing.net/th?id=OIP.Fzx8LSN_XuDpC_-Hw1mu6QHaKa&pid=Api")
